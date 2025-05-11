2638ad8bfab62a55fa06dbac974b9ca551304553
Smit085 <139530390+Smit085@users.noreply.github.com>
2025-05-11 10:55:31 +0530

    Create AppConfig.dart

---
+++ lib/core/AppConfig.dart
@@ -0,0 +1,8 @@
+class AppConfig {
+  static const String apiBaseUrl = "";
+  static const String mapSDKKey = "";
+  static const String restAPIKey = "";
+  static const String atlasClientId = "";
+  static const String atlasClientSecret = "";
+  static const String googleServerClientId = "";
+}
